318f8179aa09730fca1048e4adeaaad00ff2f40a
Jiafan Yu <joyjfy@gmail.com>
2023-05-09 22:42:51 -0700

    add shell script to reformat titles for file names

---
+++ title_formatter.sh
@@ -0,0 +1,14 @@
+# Check if the argument is provided
+if [ -z "$*" ]; then
+  echo "Usage: $0 string of words"
+  exit 1
+fi
+
+# Transform to lowercase, split by space, concatenate with underscore
+string_of_words=$(echo "$*" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+
+# Output the result and copy to system pasteboard
+echo "$string_of_words" | tee /dev/tty | pbcopy
+
+# Confirmation message
+echo "Copied to system pasteboard: $string_of_words"
